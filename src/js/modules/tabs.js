f1a99d67c9199af66615be23744fbc392b41a5c0
Andrew Afanasiev <rohteq@gmail.com>
2022-06-01 04:19:33 +0300

    5,7 paragraph(tabs)

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,41 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+    
+    function hideTabContent() {
+        content.forEach (item => {
+            item.style.display = 'none';
+        });
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+    });
+    }
+
+    function showTabContent(i = 0) {
+        content[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    //eventListener on general area of tabs,check ,did u really click on tab,then forEach not only iterable, but also its number in order,if trigger = tab - funcs
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./, '')) ||
+         target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+
+export default tabs;
